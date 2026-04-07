e559c082e7c5596117f2965ee90e7d2124642aeb
rvalieris <rvalieris@users.noreply.github.com>
2026-04-07 09:29:03 -0300

    fix for shadow dom

--- analytics.mjs
+++ analytics.mjs
@@ -1,5 +1,8 @@
 function render({ model, el }) {
   // Build your UI and append it to `el`
+  window.goatcounter = {
+      endpoint: 'https://rvalieris.goatcounter.com/count',
+  }
   var script = document.createElement("script");
   script.setAttribute('data-goatcounter', 'https://rvalieris.goatcounter.com/count');
   script.setAttribute('async','async');
